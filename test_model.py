c2430e61bd8de4a2c17a86405c1de82dab6323ba
Amir <m.amiruzzaman@gmail.com>
2018-02-08 14:51:11 -0500

    Add files via upload
    Modified segnet for getting % values for each class

--- test_model.py
+++ test_model.py
@@ -24,6 +24,7 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.image as mpimg
 
 # In[11]:
 
@@ -47,10 +48,10 @@ model.load_weights(bst_weights_path)
 
 # In[14]:
 
-image_name = sys.argv[1]
-print(image_name)
-#image_name = '2.jpg'
+#image_name = sys.argv[1]
 
+image_name = 'custom_images/8.jpg'
+print(image_name)
 
 # In[ ]:
 
@@ -116,7 +117,8 @@ def plot_images(predicted_mask):
 # In[17]:
 
 
-image = my_norm(cv2.imread('custom_images/' + image_name))
+image = my_norm(cv2.imread(image_name))
+#print(image)
 #image = my_norm(cv2.imread('custom_images/' + 2.jpg))
 #image = cv2.resize(image, (480,360))
 
@@ -132,7 +134,92 @@ image = np.rollaxis(image,2)
 image = np.expand_dims(image, axis=0)
 
 output = model.predict_proba(image)
+#print('output')
+#print(output[0])
 pred = plot_images(np.argmax(output[0],axis=1).reshape((360,480)))
+#print('pred')
+#print(pred)
+#array = np.argmax(output[0],axis=1).reshape((360,480))
+#array = pred
+#from PIL import Image
+#print(array)
+#
+#img = Image.fromarray(array.astype('uint8'))
+#img.show()
+#plt.imshow(img)
+
+#plt.plot(np.argmax(output[0],axis=1).reshape((360,480)))
+
+#plt.colorbar(orientation='vertical')
+#plt.show()
+
+
+#import numpy_indexed as npi
+#index = np.arange(pred.size) # array_3d[0].size
+#(value, index), count = npi.count((pred.flatten(), index))
+#a = np.round(pred)
+#a = pred
+data = pred.ravel()
+
+import json
+#print(json.dumps(data, default=lambda x: list(x), indent=4))
+a = json.dumps(data, default=lambda x: list(x), indent=4)
+#item_dict = json.loads(json.dumps(data, default=lambda x: list(x), indent=4))
+def unique_count(a):
+    unique, inverse = np.unique(a, return_inverse=True)
+    count = np.zeros(len(unique), np.int)
+    np.add.at(count, inverse, 1)
+    return np.vstack(( unique, count)).T
+
+#print(unique_count(data))
+b = np.unique(pred)
+c = 360*480*3
+print(c)
+print(b)
+print(np.sum(np.where(data==b[0]))/c)
+print(np.sum(np.where(data==b[1]))/c)
+print(np.sum(np.where(data==b[2]))/c)
+print(np.sum(np.where(data==b[3]))/c)
+print(np.sum(np.where(data==b[4]))/c)
+print(np.sum(np.where(data==b[5]))/c)
+print(np.sum(np.where(data==b[6]))/c)
+
+
+#print(len(item_dict['result'][0]['run']))
+with open('test.txt', 'w') as f: f.write(json.dumps(data, default=lambda x: list(x), indent=4))
+print(data.shape)
+
+#unique, counts = np.unique(a, return_counts=True)
+
+
+#print(np.asarray((unique, counts)).T)
+#print(np.bincount(a.astype(np.int64)))
+
+# =============================================================================
+# print(len(pred))
+# #b = np.reshape(pred, (1,np.product(pred.shape)))
+# b =pred.ravel()
+# b= np.asarray(b, dtype=int)
+# c = np.bincount(b)
+# print(len(b))
+# print(b.shape)
+# print(c)
+# =============================================================================
+# =============================================================================
+# import collections
+# #a = [1,1,1,1,2,2,2,2,3,3,4,5,5]
+# #counter=collections.Counter(a)
+# counter=collections.Counter(pred)
+# print(counter)
+# =============================================================================
+
+# =============================================================================
+# img = Image.fromarray(array)
+# img.save('testrgb.png')
+# img=mpimg.imread('testrgb.png')
+# plt.imshow(img)
+# plt.show()
+# =============================================================================
 
 objects_detected = len(np.unique(pred))
 print('Total Object Types detected:', objects_detected)
@@ -149,7 +236,13 @@ f.close()
 #df['objects_detected'] = 'abc'
 #df.to_csv('test.tsv', sep='\t', index=False)
 
+plt.subplot(1,2,1)
 plt.imshow(pred)
+plt.subplot(1,2,2)
+img=mpimg.imread(image_name)
+plt.imshow(img)
+
+#plt.imshow(pred)
 plt.show()
 # In[ ]:
 
